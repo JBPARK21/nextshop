2042cd202323ed1c940fd0ea898e197f581e4530
JBUPARK21 <105594950+JBUpark21@users.noreply.github.com>
2022-12-10 05:16:11 +0900

    Update hash.js

--- pages/api/crypto/hash.js
+++ pages/api/crypto/hash.js
@@ -1,31 +1,32 @@
-import { getSession } from 'next-auth/react';
-import CryptoLog from '../../../models/CryptoLog';
-import { computeHash } from '../../../utils/crypto-function';
-import db from '../../../utils/db';
+import { getSession } from 'next-auth/react'
+import CryptoLog from '../../../models/CryptoLog'
+import { computeHash } from '../../../utils/crypto-function'
+import db from '../../../utils/db'
 
 export default async function handler(req, res) {
-  const { algorithm, inputText } = req.body;
-  let hashValue = computeHash(algorithm, inputText);
+  const { algorithm, inputText } = req.body
+  let hashValue = computeHash(algorithm, inputText)
 
-  const session = await getSession({ req });
+  const session = await getSession({ req })
   if (!session) {
-    return res.status(401).send({ message: 'signin required' });
+    return res.status(401).send({ message: 'signin required' })
   }
-  const { user } = session;
-  const email = user.email;
+  const { user } = session
+  const email = user.email
 
-  const requestString = JSON.stringify(req.body);
-
-  await db.connect();
+  const requestString = JSON.stringify(req.body)
+  const resultString = JSON.stringify({ hashValue })
 
   const newCryptoLog = new CryptoLog({
     email,
     service: 'Hash',
     request: requestString,
-  });
+    result: resultString,
+  })
 
-  await newCryptoLog.save();
-  await db.disconnect();
+  await db.connect()
+  await newCryptoLog.save()
+  await db.disconnect()
 
-  res.status(200).json({ hashValue });
+  res.status(200).json({ hashValue })
 }
