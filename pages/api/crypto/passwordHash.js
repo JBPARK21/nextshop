6704c15f1efb373addfbc2103d6275346109033b
JBPARK21 <bzpark@gmail.com>
2022-12-12 11:03:08 +0900

    message

--- pages/api/crypto/passwordHash.js
+++ pages/api/crypto/passwordHash.js
@@ -1,11 +1,35 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs'
+import { getSession } from 'next-auth/react'
+import CryptoLog from '../../../models/CryptoLog'
+import db from '../../../utils/db'
 
-export default function handler(req, res) {
-  const password = req.body.password;
-  const password1 = req.body.password1;
+export default async function handler(req, res) {
+  const password = req.body.password
+  const password1 = req.body.password1
 
-  let hpassword = bcrypt.hashSync(password, 8);
-  let result = bcrypt.compareSync(password1, hpassword);
+  let hpassword = bcrypt.hashSync(password, 8)
+  let result = bcrypt.compareSync(password1, hpassword)
 
-  res.status(200).json({ result });
+  const session = await getSession({ req })
+  if (!session) {
+    return res.status(401).send({ message: 'signin required' })
+  }
+  const { user } = session
+  const email = user.email
+
+  const requestString = JSON.stringify(req.body)
+  const resultString = JSON.stringify({ result })
+
+  const newCryptoLog = new CryptoLog({
+    email,
+    service: 'Password Hash',
+    request: requestString,
+    result: resultString,
+  })
+
+  await db.connect()
+  await newCryptoLog.save()
+  await db.disconnect()
+
+  res.status(200).json({ result })
 }
--- pages/api/crypto/pbkdf2.js
+++ pages/api/crypto/pbkdf2.js
@@ -1,14 +1,38 @@
-import forge from 'node-forge';
+import { getSession } from 'next-auth/react'
+import forge from 'node-forge'
+import CryptoLog from '../../../models/CryptoLog'
+import db from '../../../utils/db'
 
-export default function handler(req, res) {
-  const password = req.body.password;
-  const salt = req.body.salt;
-  const iteration = parseInt(req.body.iteration);
-  const keyLength = parseInt(req.body.keyLength);
+export default async function handler(req, res) {
+  const password = req.body.password
+  const salt = req.body.salt
+  const iteration = parseInt(req.body.iteration)
+  const keyLength = parseInt(req.body.keyLength)
 
-  const derivedKey = forge.util.bytesToHex(
+  const key = forge.util.bytesToHex(
     forge.pkcs5.pbkdf2(password, salt, iteration, keyLength)
-  );
+  )
 
-  res.status(200).json({ key: derivedKey });
+  const session = await getSession({ req })
+  if (!session) {
+    return res.status(401).send({ message: 'signin required' })
+  }
+  const { user } = session
+  const email = user.email
+
+  const requestString = JSON.stringify(req.body)
+  const resultString = JSON.stringify({ key })
+
+  const newCryptoLog = new CryptoLog({
+    email,
+    service: 'PBKDF2',
+    request: requestString,
+    result: resultString,
+  })
+
+  await db.connect()
+  await newCryptoLog.save()
+  await db.disconnect()
+
+  res.status(200).json({ key })
 }
